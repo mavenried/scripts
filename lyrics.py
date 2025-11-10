be13ab660705846b8b8b7127516da25f9f8189b2
Joseph Chacko <josephchacko2006@gmail.com>
2025-11-10 12:30:08 +0530

    add accent, update lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -1,15 +1,29 @@
 #!/bin/env python3
-from bs4 import BeautifulSoup
+import sys
 import urllib.request
-from sys import argv
 
-song, singer = "".join(argv[1:]).strip().split("by")
-singer = "".join([i.lower() for i in singer if not i.isspace()])
+from bs4 import BeautifulSoup
+
+song, singers = [i.strip() for i in sys.stdin.read().strip().split(" by ")]
+singers = singers.split("/")
 song = "".join([i.lower() for i in song if not i.isspace()])
-url = "https://www.azlyrics.com/lyrics/" + singer + "/" + song + ".html"
+for i, singer in enumerate(singers):
+    try:
+        singer = "".join([i.lower() for i in singer if not i.isspace()])
+        url = "https://www.azlyrics.com/lyrics/" + singer + "/" + song + ".html"
 
-raw = urllib.request.urlopen(url)
-data = raw.read()
+        raw = urllib.request.urlopen(url)
+        data = raw.read()
 
-soup = BeautifulSoup(data, "html.parser")
-print(soup.find_all("div", attrs={"class": None, "id": None})[0].get_text().strip())
+        soup = BeautifulSoup(data, "html.parser")
+        print(f"From: {url}")
+        print(
+            soup.find_all("div", attrs={"class": None, "id": None})[0]
+            .get_text()
+            .strip()
+        )
+        break
+    except:
+        continue
+else:
+    print("No results :(")
